1eae3172d6fb1109b1996abd4b3393bc82e57748
Zrea198 <eillson2003@outlook.com>
2024-12-22 14:46:34 +0800

    别名路径配置

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import sum from '@/test';
+const a = sum(1, 2);
+console.log(a);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
